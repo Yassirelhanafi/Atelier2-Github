1156d78bba01ddb10f8d84cbf439bb80d682225b
yassir-elhanafi <yassirelhanafi50@gmail.com>
2025-04-16 10:15:17 +0100

    testeur

--- src/main/java/examen/examen/Controllers/EmployerController.java
+++ src/main/java/examen/examen/Controllers/EmployerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/empl")
 public class EmployerController {
     private final EmployerRepository employerRepository;
 
